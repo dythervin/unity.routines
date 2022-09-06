60e446b414c2d9abfb7cbdb0fac0253453b2e837
Dastan Dzhumabekov <dastan.wm@gmail.com>
2022-09-06 18:46:10 +0600

    Added generic actionInstr

--- Runtime/ActionInstr.cs
+++ Runtime/ActionInstr.cs
@@ -6,7 +6,7 @@ namespace Dythervin.Routines
     {
         public ActionInstr(Action action = null)
         {
-            this.action = action;
+            Init(action);
         }
 
 
@@ -39,6 +39,48 @@ namespace Dythervin.Routines
         public void Init(Action action)
         {
             this.action = action;
+        }
+    }
+
+    public class ActionInstr<T> : IRoutineInstruction
+    {
+        public T value;
+        public ActionInstr(T value, Action<T> action = null)
+        {
+            Init(action, value);
+        }
+
+
+        void IRoutineInstruction.Start()
+        {
+            action.Invoke(value);
+            ((IRoutineInstruction)this).DoneListener.OnDone();
+        }
+
+        void IRoutineInstruction.Stop()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IRoutineInstruction.Pause()
+        {
+            throw new NotImplementedException();
+        }
+
+        IInstructionDoneListener IRoutineInstruction.DoneListener { get; set; }
+
+        public IRoutineInstruction GetCopy()
+        {
+            var obj = new ActionInstr<T>(value, action);
+            return obj;
+        }
+
+        public event Action<T> action;
+
+        public void Init(Action<T> action, T value)
+        {
+            this.action = action;
+            this.value = value;
         }
     }
 }
